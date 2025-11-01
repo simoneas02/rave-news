7e42f8d2ec5b3de8fa58580b769b818c7acee49d
Simone Amorim <simoneas03@gmail.com>
2025-11-01 01:52:21 +0100

    feat: add Home page

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div>
+      <h1>🔥 HOT RAVE ALERT 🪩💋</h1>
+
+      <p>The bass was deep, the lights were wild, but nothing hit harder than the thought of you.</p>
+
+      <p>You're the rhythm my body can't stop craving, the drop that makes everything else disappear.</p>
+
+      <p>Babe, you’re the rave I never want to leave 😈💫</p>
+    </div>
+  );
+}
+
+export default Home;
