d7212554431f55e1c843432b6927072902d956e2
Simone Amorim <simoneas03@gmail.com>
2025-12-06 13:56:14 +0100

    feat: crete statys URL

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,34 @@
-const status = (req, res) => {
-  res.status(200).json({ message: "success" });
-};
+import database from "infra/database.js";
+
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseVersionResult = await database.query("SHOW server_version");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::INT FROM pg_stat_activity WHERE datname= $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      version: databaseVersionValue,
+      max_connections: parseInt(databaseMaxConnectionsValue),
+      opened_connections: databaseOpenedConnectionsValue,
+    },
+  });
+}
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,6 +3,14 @@ const STATUS_URL = "/api/v1/status";
 
 test("GET statusURL should return 200", async () => {
   const response = await fetch(`${LOCALHOST}${STATUS_URL}`);
-
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.version).toEqual("18.1");
+  expect(responseBody.dependencies.max_connections).toEqual(100);
+  expect(responseBody.dependencies.opened_connections).toEqual(1);
 });
