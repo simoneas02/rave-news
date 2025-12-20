98699a831f6ead9562b9ff9ebb0b23522aacb5f4
Simone Amorim <simoneas03@gmail.com>
2025-12-20 22:46:45 +0100

    feat: add eslint job in linting Github Actions

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import prettier from "eslint-config-prettier/flat";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  prettier,
+  {
+    files: ["**/*.{js,jsx}"],
+    settings: {
+      next: {
+        rootDir: ".",
+      },
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
