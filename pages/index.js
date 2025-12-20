f04814c06c36b21680c5f510f6f287c839109575
Simone Amorim <simoneas03@gmail.com>
2025-12-20 20:17:50 +0100

    fix: use the correct code style to index page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
 
 function Home() {
   return (
@@ -6,17 +6,20 @@ function Home() {
       <div className={styles.container}>
         <h1 className={styles.container__title}>🔥 Heyyyy my BABE 💋</h1>
 
-        <p className={styles.container__text}>
-          Look at YOUUUU!!! 🤩🤩🤩
-        </p>
+        <p className={styles.container__text}>Look at YOUUUU!!! 🤩🤩🤩</p>
 
+        <p className={styles.container__text}>Look at YOUUUU!!! 👀👀👀</p>
         <p className={styles.container__text}>
-          Look at YOUUUU!!! 👀👀👀
+          You're so fucking beautiful 🫠{" "}
         </p>
-        <p className={styles.container__text}>You're so fucking beautiful  🫠 </p>
-        <p className={styles.container__text}>And <strong className={styles.container__text_strong}>PLEASE</strong> put that beautiful smile on your face and remember: I only want to see you happy, filled with joy and love! 😍 </p>
-        <p className={styles.container__text}>I can't wait to see you again  💟 </p>
-
+        <p className={styles.container__text}>
+          And <strong className={styles.container__text_strong}>PLEASE</strong>{" "}
+          put that beautiful smile on your face and remember: I only want to see
+          you happy, filled with joy and love! 😍{" "}
+        </p>
+        <p className={styles.container__text}>
+          I can't wait to see you again 💟{" "}
+        </p>
       </div>
     </div>
   );
