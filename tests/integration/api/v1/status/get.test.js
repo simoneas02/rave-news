5253a5303fe6d72e3843bdb6582cdfd254a8bd09
Simone Amorim <simoneas03@gmail.com>
2025-12-26 15:43:42 +0100

    test: improve all tests descriptions

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,16 +5,20 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET statusURL should return 200", async () => {
-  const response = await fetch(STATUS_URL);
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieven current system status", async () => {
+      const response = await fetch(STATUS_URL);
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.version).toEqual("18.1");
-  expect(responseBody.dependencies.max_connections).toEqual(100);
-  expect(responseBody.dependencies.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.version).toEqual("18.1");
+      expect(responseBody.dependencies.max_connections).toEqual(100);
+      expect(responseBody.dependencies.opened_connections).toEqual(1);
+    });
+  });
 });
