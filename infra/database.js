8a48735c810a2b4c47479607029c002d27e06373
Simone Amorim <simoneas03@gmail.com>
2026-01-07 22:49:31 +0100

    feat: use `ServiceError` in `database`

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "errors/serviceError";
 
 async function query(queryObject) {
   let client;
@@ -10,8 +11,11 @@ async function query(queryObject) {
 
     return result;
   } catch (error) {
-    console.log(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Error connecting to the database or in the query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
@@ -21,18 +25,26 @@ const getSSLValues = () =>
   process.env.NODE_ENV === "production" ? true : false;
 
 async function getNewClient() {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
+  try {
+    const client = new Client({
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      ssl: getSSLValues(),
+    });
 
-  await client.connect();
+    await client.connect();
 
-  return client;
+    return client;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      message: "Database connection error",
+      cause: error,
+    });
+    throw serviceErrorObject;
+  }
 }
 
 const database = {
