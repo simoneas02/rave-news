b981724bfb0d4f9b0f9bf1fe0e9ce8ae7b2d746d
Simone Amorim <simoneas03@gmail.com>
2026-03-24 17:17:03 +0100

    feat: add `email.j` infra module

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,8 +6,11 @@ import { STATUS_URL } from "./integration/api/v1/status/get.test";
 import user from "models/user";
 import session from "models/session";
 
+const EMAIL_HTTP_URL = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
+
 async function waitForAllServices() {
   await waitForWebServices();
+  await waitForEmailServices();
 
   async function waitForWebServices() {
     return retry(fetchStatusPage, {
@@ -17,6 +20,22 @@ async function waitForAllServices() {
 
     async function fetchStatusPage() {
       const response = await fetch(STATUS_URL);
+      const isResponseOk = response.status === 200;
+
+      if (!isResponseOk) {
+        throw Error();
+      }
+    }
+  }
+
+  async function waitForEmailServices() {
+    return retry(fetchEmailPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchEmailPage() {
+      const response = await fetch(EMAIL_HTTP_URL);
       const isResponseOk = response.status === 200;
 
       if (!isResponseOk) {
@@ -49,12 +68,34 @@ async function createSession(userId) {
   return await session.create(userId);
 }
 
+async function deleteAllEmails() {
+  await fetch(`${EMAIL_HTTP_URL}/messages`, { method: "DELETE" });
+}
+
+async function getLastEmail() {
+  const emailListReponse = await fetch(`${EMAIL_HTTP_URL}/messages`);
+  const emailListBody = await emailListReponse.json();
+
+  const lastEmailItem = emailListBody.at(-1);
+
+  const lastEmailResponse = await fetch(
+    `${EMAIL_HTTP_URL}/messages/${lastEmailItem?.id}.plain`,
+  );
+
+  const emailTextBody = await lastEmailResponse.text();
+  lastEmailItem.text = emailTextBody;
+
+  return lastEmailItem;
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
   runPendingMigrations,
   createUser,
   createSession,
+  deleteAllEmails,
+  getLastEmail,
 };
 
 export default orchestrator;
